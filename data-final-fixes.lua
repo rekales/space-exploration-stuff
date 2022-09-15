5acd27fd3ca7b8f6db1f1c18cb701828ba9fd8ad
Krei <criketgun@gmail.com>
2022-09-15 23:50:31 +0800

    added config

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,5 +3,4 @@
 if settings.startup["more-wdc-ammo"].value then require("prototypes/more_wdc_ammo.lua") end
 if settings.startup["simplified-delivery-capsule"].value then require("prototypes/simplified_delivery_capsule") end
 if settings.startup["neutral-core-landfill"].value then require("prototypes/neutral_core_landfill.lua") end
-
-require("prototypes/cannonable_nuclear_fuel.lua")
+if settings.startup["cannonable-nuclear-fuel"].value then require("prototypes/cannonable_nuclear_fuel.lua") end
