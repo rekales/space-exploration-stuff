7a2576a2cc8f1e68e6980f7b45d79e012198696b
Krei <criketgun@gmail.com>
2022-09-29 13:19:27 +0800

    bugfix on cancel_pollution_emmision

--- scripts/cancel_pollution_emission.lua
+++ scripts/cancel_pollution_emission.lua
@@ -2,7 +2,9 @@ local data_util = require("data_util")
 
 local function on_chunk_generated(event)
     local surface = event.surface
-    if surface.map_gen_settings.autoplace_controls["enemy-base"].size == -1
+    if surface.map_gen_settings.autoplace_controls
+    and surface.map_gen_settings.autoplace_controls["enemy-base"]
+    and surface.map_gen_settings.autoplace_controls["enemy-base"].size == -1
     then
         surface.create_entity{name=data_util.mod_prefix .. "pollution-absorber", position={x=event.position.x*32, y=event.position.y*32}}
     end
@@ -19,6 +21,8 @@ local function on_gui_click(event)
 
         local surface = game.get_surface(surface_name)
         if surface
+        and surface.map_gen_settings.autoplace_controls
+        and surface.map_gen_settings.autoplace_controls["enemy-base"]
         and surface.map_gen_settings.autoplace_controls["enemy-base"].size == -1
         then
             local chunk = surface.get_random_chunk()
@@ -38,7 +42,9 @@ end
 Event.add_config_changed_listener(function()
     for _,surface in pairs(game.surfaces)
     do
-        if surface.map_gen_settings.autoplace_controls["enemy-base"].size == -1
+        if surface.map_gen_settings.autoplace_controls
+        and surface.map_gen_settings.autoplace_controls["enemy-base"]
+        and surface.map_gen_settings.autoplace_controls["enemy-base"].size == -1
         then
             local chunk = surface.get_random_chunk()
             if not surface.find_entity(data_util.mod_prefix .. "pollution-absorber", {chunk.x*32+.5, chunk.y*32+.5})
